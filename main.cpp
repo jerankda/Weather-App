41d003427ac6330252166bee27cdb9312e61d920
Daniel Jeranko <119113564+jerankda@users.noreply.github.com>
2022-12-11 00:57:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 #include <curl/curl.h> // curl library
 
 int main() {
-  // Your OpenWeatherMap API key
+  
   const std::string API_KEY = "your_api_key_here";
 
-  // Ask the user for their location
+
   std::string location;
   std::cout << "Enter your location: ";
   std::cin >> location;
@@ -19,7 +19,7 @@ int main() {
   CURL *curl = curl_easy_init();
   curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
   curl_easy_perform(curl);
-
+  
   // Parse the JSON response from the OpenWeatherMap API
   nlohmann::json weatherData;
   curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, [&](const char *in, std::size_t size, std::size_t num, std::string *out) {
@@ -27,11 +27,9 @@ int main() {
     return size * num;
   });
 
-  // Extract the current temperature and weather description from the JSON data
   int temperature = weatherData["main"]["temp"].get<int>();
   std::string description = weatherData["weather"][0]["description"].get<std::string>();
 
-  // Print the current temperature and weather description to the user
   std::cout << "The current weather in " << location << " is " << description << " with a temperature of " << temperature << " degrees." << std::endl;
 
   return 0;
